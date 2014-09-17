60c63664e6019af780d493b74f7d9b8ac600bde7
Jared.Hofer <Jared.Hofer@MU-C3-14.mullen.pvt.k12.co.us>
2014-09-17 13:44:09 -0600

    started making locations class

---
+++ Sword Art Online/src/Locations.java
@@ -0,0 +1,15 @@
+
+public class Locations
+	{
+	Mob mobInRoom;
+	//this will create a room
+	public static void createRoom()
+		{
+		spawnMobs();
+		}
+	//this will spawn Mobs according to the players level
+	public static void spawnMobs()
+		{
+		
+		}
+	}
